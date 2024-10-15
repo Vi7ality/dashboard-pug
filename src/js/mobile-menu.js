b9982ee66991b33efc7ff7e35c6d520569c74e25
Vitalii S. <svitalii138@gmail.com>
2024-10-15 15:18:28 +0300

    mobile styles added

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,22 +2,16 @@
   const mobileMenu = document.querySelector(".js-menu-container");
   const openMenuBtn = document.querySelector(".js-open-menu");
   const closeMenuBtn = document.querySelector(".js-close-menu");
-  //   const overlay = document.querySelector(".js-overlay-modal");
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute("aria-expanded") === "true" || false;
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
     mobileMenu.classList.toggle("is-open");
-    // overlay.classList.toggle("active");
-
-    // const scrollLockMethod = !isMenuOpen ? "disableBodyScroll" : "enableBodyScroll";
-    // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener("click", toggleMenu);
   closeMenuBtn.addEventListener("click", toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
   window.addEventListener("resize", closeMenu);
 
   function closeMenu() {
@@ -29,12 +23,4 @@
       bodyScrollLock.enableBodyScroll(document.body);
     }
   }
-
-  /*window.matchMedia('(min-width: 1200px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    overlay.classList.remove('active');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });*/
 })();
